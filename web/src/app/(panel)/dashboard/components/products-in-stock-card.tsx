40b3dadcb093962a229c6412da29c1f924959df0
henrique1501 <vicsam7716@gmail.com>
2023-12-26 06:33:25 +0000

    page: dashboard base style started

---
+++ web/src/app/(panel)/dashboard/components/products-in-stock-card.tsx
@@ -0,0 +1,20 @@
+import { PackageCheck } from 'lucide-react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function ProductsInStockCard() {
+  return (
+    <Card>
+      <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-base font-semibold">Products</CardTitle>
+        <PackageCheck className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent className="space-y-1">
+        <span className="text-2xl font-bold tracking-tight">156</span>
+        <p className="text-xs text-muted-foreground">
+          <span className="text-emerald-500 dark:text-emerald-400">+6%</span> from last month
+        </p>
+      </CardContent>
+    </Card>
+  )
+}
--- web/src/app/(panel)/dashboard/page.tsx
+++ web/src/app/(panel)/dashboard/page.tsx
@@ -1,7 +1,21 @@
+import { CalendarDateRangePicker } from './components/calendar-date-range-picker';
+import { MonthOrdersAmountCard } from './components/month-orders-amount-card';
+import { ProductsInStockCard } from './components/products-in-stock-card';
+
 export default function Dashboard() {
   return (
-    <main>
-      <h1>Dashboard</h1>
-    </main>
+    <section className="p-6 flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <CalendarDateRangePicker />
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        <MonthOrdersAmountCard />
+        <ProductsInStockCard />
+      </div>
+
+    </section>
   )
 }
