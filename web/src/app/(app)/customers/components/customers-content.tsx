26ed96231adbcfe69c8aaefeea8859f20175238a
henrique1501 <vicsam7716@gmail.com>
2024-01-05 01:14:20 +0000

    page: customers updated

--- web/src/app/(app)/customers/components/customers-content.tsx
+++ web/src/app/(app)/customers/components/customers-content.tsx
@@ -1,13 +1,6 @@
 'use client'
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import {
   Select,
   SelectContent,
   SelectItem,
@@ -23,9 +16,10 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { MoreVertical, Pen, Trash } from 'lucide-react'
+import { Pen, Trash } from 'lucide-react'
 import Link from 'next/link'
 import { CustomersForm } from './customers-form'
+import { DeleteCustomerButton } from './delete-customer-button'
 
 export function CustomersContent() {
   return (
@@ -84,6 +78,7 @@ export function CustomersContent() {
             <TableHead>Registered in</TableHead>
             <TableHead>Updated at</TableHead>
             <TableHead></TableHead>
+            <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -97,35 +92,19 @@ export function CustomersContent() {
               <TableCell>28/12/2023</TableCell>
               <TableCell>29/12/2023</TableCell>
               <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <MoreVertical className="h-5 w-5 stroke-zinc-900" />
-                  </DropdownMenuTrigger>
-
-                  <DropdownMenuContent className="w-44" align="end" forceMount>
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem asChild>
-                        <Link
-                          href={`/customers/123/edit`}
-                          className="flex items-center gap-2 hover:underline cursor-pointer group"
-                        >
-                          <Pen className="h-4 w-4 stroke-muted-foreground group-hover:stroke-zinc-900" />
-                          <span className="text-sm leading-none text-muted-foreground group-hover:text-zinc-900">
-                            Edit
-                          </span>
-                        </Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem className="group">
-                        <button className="flex items-center gap-2">
-                          <Trash className="h-4 w-4 stroke-muted-foreground group-hover:stroke-zinc-900" />
-                          <span className="text-sm leading-none text-muted-foreground group-hover:text-zinc-900">
-                            Delete
-                          </span>
-                        </button>
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                <Link
+                  href={`/customers/123/edit`}
+                  className="flex items-center gap-2 cursor-pointer group"
+                >
+                  <Pen className="h-4 w-4 stroke-muted-foreground group-hover:stroke-zinc-900" />
+                </Link>
+              </TableCell>
+              <TableCell>
+                <DeleteCustomerButton customerName={'Jhon doe'}>
+                  <button className="flex items-center gap-2">
+                    <Trash className="h-4 w-4 stroke-muted-foreground group-hover:stroke-zinc-900" />
+                  </button>
+                </DeleteCustomerButton>
               </TableCell>
             </TableRow>
           ))}
---
+++ web/src/app/(app)/customers/components/delete-customer-button.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { ReactNode } from 'react'
+
+interface DeleteCustomerButtonProps {
+  customerName: string
+  children: ReactNode
+}
+
+export function DeleteCustomerButton({
+  customerName,
+  children,
+}: DeleteCustomerButtonProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently the customer:{' '}
+            <strong>{customerName}</strong>.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Confirm</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
