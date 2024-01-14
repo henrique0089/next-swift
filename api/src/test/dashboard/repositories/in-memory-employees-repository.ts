e466a767d9e9a83bea1c09e093b26c9e440b0a1a
Henrique Monteiro <henriquemonteiro037@gmail.com>
2024-01-14 02:38:54 +0000

    tests: hire-employee-usecase finished

---
+++ api/src/test/dashboard/repositories/in-memory-employees-repository.ts
@@ -0,0 +1,52 @@
+import { Employee } from '@app/dashboard/entities/employee'
+import { EmployeesRepository } from '@app/dashboard/repositories/employees-repository'
+
+export class InMemoryEmployeesRepository implements EmployeesRepository {
+  public employees: Employee[] = []
+
+  async findAll(): Promise<Employee[]> {
+    return this.employees
+  }
+
+  async findById(id: string): Promise<Employee | null> {
+    const employee = this.employees.find((employee) => employee.id === id)
+
+    if (!employee) {
+      return null
+    }
+
+    return employee
+  }
+
+  async findByEmail(email: string): Promise<Employee | null> {
+    const employee = this.employees.find((employee) => employee.email === email)
+
+    if (!employee) {
+      return null
+    }
+
+    return employee
+  }
+
+  async create(employee: Employee, roleId: string): Promise<void> {
+    this.employees.push(employee)
+  }
+
+  async save(employee: Employee): Promise<void> {
+    const employeeIndex = this.employees.findIndex(
+      (data) => data.id === employee.id,
+    )
+
+    if (employeeIndex !== -1) {
+      this.employees[employeeIndex] = employee
+    }
+  }
+
+  async delete(employeeId: string): Promise<void> {
+    const employeeIndex = this.employees.findIndex(
+      (employee) => employee.id === employeeId,
+    )
+
+    this.employees.splice(employeeIndex, 1)
+  }
+}
