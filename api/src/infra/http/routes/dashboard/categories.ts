9bed91afab3060847645d3385258caa168e0eacb
Henrique Monteiro <henriquemonteiro037@gmail.com>
2024-01-19 19:14:43 +0000

    feat: delete category finished

--- api/src/infra/http/routes/dashboard/categories.ts
+++ api/src/infra/http/routes/dashboard/categories.ts
@@ -1,8 +1,11 @@
 import { AddCategoryController } from '@infra/http/controllers/dashboard/categories/add-category-controller'
+import { DeleteCategoryController } from '@infra/http/controllers/dashboard/categories/delete-category-controller'
 import { FastifyInstance } from 'fastify'
 
 const addCategoryController = new AddCategoryController()
+const deleteCategoryController = new DeleteCategoryController()
 
 export async function categoriesRoutes(app: FastifyInstance) {
   app.post('/categories', addCategoryController.handle)
+  app.delete('/categories/:categoryId/delete', deleteCategoryController.execute)
 }
