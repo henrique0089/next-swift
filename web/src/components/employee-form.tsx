15c3ad5571c7bb83578d5ac47a5f0dbe460d95ac
Carlos henrique <henriquemonteiro714@gmail.com>
2024-03-17 17:52:11 +0000

    component: sign in form updated

--- web/src/components/employee-form.tsx
+++ web/src/components/employee-form.tsx
@@ -7,19 +7,12 @@ import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Button } from './ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from './ui/form'
 import { Input } from './ui/input'
+import { Label } from './ui/label'
 
 const employeeAuthFormSchema = z.object({
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(6),
 })
 
 type EmployeeAuthFormValues = z.infer<typeof employeeAuthFormSchema>
@@ -27,7 +20,11 @@ type EmployeeAuthFormValues = z.infer<typeof employeeAuthFormSchema>
 export function EmployeeAuthForm() {
   const { isLoaded, signIn, setActive } = useSignIn()
 
-  const form = useForm<EmployeeAuthFormValues>({
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<EmployeeAuthFormValues>({
     resolver: zodResolver(employeeAuthFormSchema),
   })
 
@@ -50,65 +47,48 @@ export function EmployeeAuthForm() {
     }
   }
 
-  const {
-    handleSubmit,
-    formState: { isSubmitting },
-  } = form
+  const emailErr = errors.email?.message
+  const passErr = errors.password?.message
 
   return (
-    <Form {...form}>
-      <form onSubmit={handleSubmit(handleSignIn)} className="space-y-2 w-full">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  placeholder="name@example.com"
-                  type="email"
-                  disabled={isSubmitting}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
+    <form onSubmit={handleSubmit(handleSignIn)} className="space-y-2 w-full">
+      <div className="space-y-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="jhondoe@gmail.com"
+          disabled={isSubmitting}
+          {...register('email')}
         />
+        {emailErr && <span>{emailErr}</span>}
+      </div>
 
-        <div>
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    placeholder="your password"
-                    type="password"
-                    disabled={isSubmitting}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+      <div>
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="your password"
+            disabled={isSubmitting}
+            {...register('password')}
           />
-          <Link
-            href="/recovery-password"
-            className="text-right mt-2 w-full block text-sm text-muted-foreground hover:underline underline-offset-4 hover:text-primary"
-          >
-            Forgot password?
-          </Link>
+          {passErr && <span>{passErr}</span>}
         </div>
 
-        <Button disabled={isSubmitting} className="w-full">
-          {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          Sign In
-        </Button>
-      </form>
-    </Form>
+        <Link
+          href="/recovery-password"
+          className="text-right mt-2 w-full block text-sm text-muted-foreground hover:underline underline-offset-4 hover:text-primary"
+        >
+          Forgot password?
+        </Link>
+      </div>
+
+      <Button disabled={isSubmitting} className="w-full">
+        {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+        Sign In
+      </Button>
+    </form>
   )
 }
